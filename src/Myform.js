75e335f7810e64a9739efea2811148eadca129bb
abd-gh <abdgh681@gmail.com>
2022-05-19 10:27:58 +0200

    version2

--- src/Myform.js
+++ src/Myform.js
@@ -2,8 +2,7 @@ import './App.css';
 let x=0;
 function Myform(){
   return(
-      <>
-      
+      <>     
       <div className='InputForm'>
         <input className='InputFeild' id="name" type="text" placeholder='Name'/>
         <input className='InputFeild' id="age" type="text" placeholder='Age' />
@@ -21,6 +20,11 @@ function addPerson(){
     let age=document.getElementById('age').value;
     let sex=document.getElementById('sex').value;
     
+    //create row for each person
+    const ul = document.createElement('ul');
+    ul.className="tbl1";
+
+    // column
     const li = document.createElement('li');
     li.innerHTML = name;
     li.className="lis";
@@ -36,36 +40,26 @@ function addPerson(){
     const btn=document.createElement('button');
     btn.id=x;
     btn.innerHTML+="x";
-    
+    btn.className="btndel";
 
+    ul.append(li, li2, li3, btn);
+    
     btn.onclick = function(event) {
         li.style.display="none";
         li2.style.display="none";
         li3.style.display="none";
         btn.style.display="none";
+        ul.style.display="none";
       }
-    // Add new element to our unordered list:
-    document.querySelector('#DataRowt').append(li);
-    document.querySelector('#DataRowt').append(li2);
-    document.querySelector('#DataRowt').append(li3);
-    document.querySelector('#DataRowt').append(btn)
 
-           //
-           //let result="<tr class='Row' id='"+(x)+"'>";
-           // result+="<td>"+name+"</td>";
-          //  result+="<td>"+age+"</td>";
-          //  result+="<td>"+sex+"</td>";
-          //  result+="<td><button onClick="+{del(x)}+">x</button></td>";
-          //  result+="</tr>"
-       
-        //document.getElementById("DataRowt").innerHTML+=result;
+    document.querySelector('#DataRowt').append(ul);
+    document.getElementById('name').value="";
+    document.getElementById('age').value="";
+    document.getElementById('sex').value="";
+    
 
 }
- 
-  function del(id){ 
-    
-     alert (id) ;
-  } 
+  
 
 
 export default Myform
